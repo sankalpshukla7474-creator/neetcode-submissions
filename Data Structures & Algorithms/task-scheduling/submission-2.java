1cfe021a6d7436ff5eec96be64413073fac60767
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-07-04 14:49:14 +0530

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character,Integer> hm = new HashMap<>() ;
+        for(char ch : tasks){
+            hm.put(ch,hm.getOrDefault(ch,0)+1) ;
+        }
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->b.B - a.B) ;
+        for(char ch : hm.keySet()){
+            Pair p = new Pair(ch,hm.get(ch)) ;
+            pq.offer(p) ;
+        }
+        Pair p = pq.poll() ;
+        int max = p.B , count = 1 ;
+        while(!pq.isEmpty()){
+            Pair q = pq.poll() ;
+            if(q.B == max){
+                count++ ;
+            }
+            else{
+                break ;
+            }
+        }
+        count-- ;
+        int ans = ((max) * (n+1)) - n + count ;
+        return Math.max(ans,tasks.length) ;
+    }
+    class Pair{
+        char A ;
+        int B ; 
+        Pair(char A , int B){
+            this.A = A ;
+            this.B = B ;
+        }
+    }
+}
