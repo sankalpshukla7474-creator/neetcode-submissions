280d92f050a5a66fab237a6f9cf4d3f1b1b768d0
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-07-14 12:09:28 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.java
@@ -0,0 +1,26 @@
+class Solution {
+    Boolean[] dp = new Boolean[201];
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return pass(s, 0, wordDict);
+    }
+
+    public boolean pass(String s, int i, List<String> wD) {
+        if (i == s.length()) return true;
+        if (i > s.length()) return false;
+
+        if (dp[i] != null) return dp[i];
+
+        for (int j = i; j < s.length(); j++) {
+            String ptr = s.substring(i, j + 1);
+
+            if (wD.contains(ptr)) {
+                if (pass(s, j + 1, wD)) {
+                    return dp[i] = true;
+                }
+            }
+        }
+
+        return dp[i] = false;
+    }
+}
