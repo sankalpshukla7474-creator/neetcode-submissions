e0a6040f18a07e4057edb6e005268c4768e9993a
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-07-14 12:05:31 +0530

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return pass(s,0,wordDict) ;
+    }
+    public boolean pass(String s , int i , List<String> wD){
+        if(i == s.length()) return true ;
+        if(i > s.length()) return false ;
+        for(int j = i ; j < s.length() ; j++){
+            String ptr = s.substring(i,j+1) ;
+            if(wD.contains(ptr)){
+                if(pass(s,j+1,wD)){
+                    return true ;
+                }
+            }
+        }
+        return false ;
+    }
+}
