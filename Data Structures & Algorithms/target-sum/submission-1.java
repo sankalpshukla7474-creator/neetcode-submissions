a1990db59237b03c06da1eda309cb66fae3c8183
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-07-16 10:34:13 +0530

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int range = 0 ;
+        for(int x : nums){
+            range += x ;
+        }
+        if(target > range) return 0 ;
+        int tar = (range + target) / 2 ;
+        return pass(nums,tar,0) ;
+    }
+    public int pass(int[] nums , int tar , int i){
+        if(i == nums.length){
+            if(tar == 0) return 1 ;
+            else return 0 ;
+        }
+        int a = pass(nums,tar-nums[i],i+1) ;
+        int b = pass(nums,tar,i+1) ;
+        return (a+b) ;
+    }
+}
