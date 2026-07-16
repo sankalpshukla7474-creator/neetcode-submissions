d16aae7490da01ef887201413ef60c40131dfe61
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-07-16 18:23:42 +0530

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Character> st = new Stack<>() ;
+        int count = 0 ;
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                st.push(ch) ;
+            }
+            else if(ch == '*'){
+                count++ ;
+            }
+            else{
+                if(st.isEmpty()) return false ;
+                char ph = st.peek() ;
+                if(ph == '(' ){
+                    continue ;
+                }
+                else if(count > 0){
+                    count-- ;
+                }
+                else{
+                    return false ;
+                }
+            }
+        }
+        return count >= st.size() ? true : false ;
+    }
+}
