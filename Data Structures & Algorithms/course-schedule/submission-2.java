37f2d6819d64f8abaf38aae476c0a513563547fe
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-07-29 11:16:37 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    List<List<Integer>> adj = new ArrayList<>() ;
+    public boolean canFinish(int num, int[][] pre) {
+        Set<Integer> st = new HashSet<>() ;
+        if(pre.length == 0) return true ;
+        for(int i = 0 ; i <= num ; i++){
+            adj.add(new ArrayList<>()) ;
+        }
+        int[] indeg = new int[num+1] ;
+        for(int i = 0 ; i < pre.length ; i++){
+            int u = pre[i][1] ;
+            int v = pre[i][0] ;
+            adj.get(u).add(v) ;
+            st.add(u) ; st.add(v) ;
+            indeg[v]++ ;
+        }
+        Queue<Integer> q = new ArrayDeque<>() ;
+        for(int i = 1 ; i <= num ; i++){
+            if(indeg[i] == 0){
+                q.offer(i) ;
+            }
+        }
+        int count = 1 ;
+        while(!q.isEmpty()){
+            int node = q.poll() ;
+            for(int neigh : adj.get(node)){
+                indeg[neigh]-- ;
+                if(indeg[neigh] == 0){
+                    q.offer(neigh) ;
+                    count++ ;
+                }
+            }
+        }
+        return st.size() == count ;
+    }
+}
